9b55533a086489b3c079f3bf465276e11d65237b
huycans <huycan_lk@yahoo.com.vn>
2020-11-21 11:44:17 -0500

    Setup path to join game

--- app.js
+++ app.js
@@ -5,10 +5,20 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var mongoose = require("mongoose");
 var passport = require("passport");
+var https = require('https');
+var fs = require('fs');
+var path = require('path');
+const WebSocket = require('ws');
+const Game = require('./game');
+// var app = require("./app")
+var debug = require('debug')('swe681:server');
+
 
 var welcomeRouter = require('./routes/welcome');
 var userRouter = require('./routes/users');
 var config = require("./config");
+// const gameRouter = require("./routes/game");
+var cors = require("./routes/cors");
 
 var app = express();
 
@@ -42,9 +52,50 @@ connect.then(
   }
 );
 
+//https server
+var httpsServer = https.createServer({
+  key: fs.readFileSync(path.win32.resolve(__dirname, "./secret/server.key")),
+  cert: fs.readFileSync(path.win32.resolve(__dirname, './secret/server.cert'))
+}, app);
+
+//establish websocket
+const wss = new WebSocket.Server({ server: httpsServer });
+const rummy = new Game(wss);
+
+// Ignore Socket Errors
+wss.on('error', () => console.log('*errored*'));
+wss.on('close', () => console.log('*disconnected*'));
+
 //API paths on the server
 app.use('/welcome', welcomeRouter);
 app.use('/user', userRouter);
+
+//path to game
+app.get('/join/:lobby', cors.cors, (req, res) => {
+  let code = req.params.lobby;
+  if (rummy.addLobby(code)) {
+    // res.redirect('/game/' + req.params.lobby + '/' + rummy.lobbies[code].token);
+    res.setHeader("Content-Type", "application/json");
+    res.statusCode = 200;
+    res.json({ message: "Lobby created at " + rummy.lobbies[code].token });
+  } else {
+    // res.redirect('/');
+    console.log("Error creating lobby");
+    res.statusCode = 500;
+    res.json({ message: "Error creating lobby" });
+  }
+});
+
+// app.get('/play/:lobby/:token', cors.cors, (req, res) => {
+//   let code = "" + req.params.lobby,
+//     token = req.params.token;
+//   if (req.params.token && rummy.lobbies[code] && rummy.lobbies[code].token == token) {
+//     res.sendFile(__dirname + '/public/game.html');
+//   } else {
+//     res.redirect('/');
+//   }
+// });
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -61,6 +112,87 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+
+/**
+ * Get port from environment and store in Express.
+ */
+const defaultPort = "3000";
+var port = normalizePort(process.env.PORT || defaultPort);
+app.set('port', port);
+
+
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+
+httpsServer.on('error', onError);
+httpsServer.on('listening', onListening);
+
+// Start Server
+httpsServer.listen(port, () => {
+  console.log("Listening on port " + port + "...");
+});
+/**
+ * Normalize a port into a number, string, or false.
+ */
+
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+
+/**
+ * Event listener for HTTPs server "error" event.
+ */
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  var bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+function onListening() {
+  var addr = httpsServer.address();
+  var bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+  debug('Listening on ' + bind);
+}
 
 
 module.exports = app;
--- websocket.js
+++ websocket.js
@@ -1,32 +1,32 @@
-const WebSocket = require('ws');
+// const WebSocket = require('ws');
 
-//HTTPS server
-var httpsServer = require("./server");
-var app = require("./app")
-const Game = require('./game');
+// //HTTPS server
+// var httpsServer = require("./server");
+// var app = require("./app")
+// const Game = require('./game');
 
-const wss = new WebSocket.Server({ server });
-const rummy = new Game(wss);
+// const wss = new WebSocket.Server({ server });
+// const rummy = new Game(wss);
 
-// Ignore Socket Errors
-wss.on('error', () => console.log('*errored*'));
-wss.on('close', () => console.log('*disconnected*'));
+// // Ignore Socket Errors
+// wss.on('error', () => console.log('*errored*'));
+// wss.on('close', () => console.log('*disconnected*'));
 
-app.get('/join/:lobby', (req, res) => {
-  let code = req.params.lobby;
-  if (rummy.addLobby(code)) {
-    res.redirect('/game/' + req.params.lobby + '/' + rummy.lobbies[code].token);
-  } else {
-    res.redirect('/');
-  }
-});
+// app.get('/join/:lobby', (req, res) => {
+//   let code = req.params.lobby;
+//   if (rummy.addLobby(code)) {
+//     res.redirect('/game/' + req.params.lobby + '/' + rummy.lobbies[code].token);
+//   } else {
+//     res.redirect('/');
+//   }
+// });
 
-app.get('/game/:lobby/:token', (req, res) => {
-  let code = "" + req.params.lobby,
-    token = req.params.token;
-  if (req.params.token && rummy.lobbies[code] && rummy.lobbies[code].token == token) {
-    res.sendFile(__dirname + '/public/game.html');
-  } else {
-    res.redirect('/');
-  }
-});
+// app.get('/game/:lobby/:token', (req, res) => {
+//   let code = "" + req.params.lobby,
+//     token = req.params.token;
+//   if (req.params.token && rummy.lobbies[code] && rummy.lobbies[code].token == token) {
+//     res.sendFile(__dirname + '/public/game.html');
+//   } else {
+//     res.redirect('/');
+//   }
+// });
