5f68508dc461efacb72d3e8a21216a838ff8cb5b
huycans <huycan_lk@yahoo.com.vn>
2020-12-20 12:18:55 -0500

    Remove views

--- server/app.js
+++ server/app.js
@@ -23,7 +23,6 @@ var { verifyUser } = require("./authenticate");
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
